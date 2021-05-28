9304e3f6a1c5e8b561ab50732923cea7a40a7748
Shinde Jayesh Arun <shindejayesharun@gmail.com>
2021-05-28 23:02:36 +0530

    comment added

--- app/src/main/java/com/shindejayesharun/jetpack_compose/examples/SimpleTextActivity.kt
+++ app/src/main/java/com/shindejayesharun/jetpack_compose/examples/SimpleTextActivity.kt
@@ -10,13 +10,28 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.shindejayesharun.jetpack_compose.examples.ui.theme.Jetpack_composeTheme
 
+/**
+ * ComponentActivity is base activity of component building, this class extends with
+ * androidx.core.app.ComponentActivity
+ * with implemented ContextAware, LifecycleOwner, ViewModelStoreOwner,
+ * HasDefaultViewModelProviderFactory, SavedStateRegistryOwner, OnBackPressedDispatcherOwner,
+ * ActivityResultRegistryOwner, ActivityResultCaller
+ */
 class SimpleTextActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        /**
+         * this is replace of set content of xml file by setContentView(R.layout.activity_main)
+         * by setContent you set this layout is parent of this activity
+         */
         setContent {
+            /**
+             * jetpack theme used by them include color, shape, type
+             */
             Jetpack_composeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
+                    //Greeting is composable function part of the activity and act as a widget
                     Greeting("Android")
                 }
             }
@@ -24,11 +39,22 @@ class SimpleTextActivity : ComponentActivity() {
     }
 }
 
+/**
+ * @Composable function is called within scope of the composable function
+ * this contains textview with @param name as a parameter of function
+ */
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
+/**
+ * This is best way to preview your layout without build code and waiting for gradle build and
+ * emulator to setup them. Composable used used for transformation of application data to tree
+ * or hierarchy,
+ * composable preview not accept any parameter if you want to pass some data to preview within
+ * composable you need to overlap composable function with another composable function
+ */
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
